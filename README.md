2d4af605987aaf35aadc174a17bff3fccc19ec02
Yoseph Anderson <yrganderson@gmail.com>
2016-12-13 23:45:10 -0700

    more explaination in readme

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Redux Weather App
 
-Interested in learning [Redux](https://www.udemy.com/react-redux/)?
+React-Redux application used to look up weather for locations around the United States. Weather predictions are graphed and displayed using an external graphing library.
+
 
 # Getting Started
 
@@ -8,3 +9,8 @@ Interested in learning [Redux](https://www.udemy.com/react-redux/)?
 npm install
 npm start
 ```
+Once server is started, type a city name into the search bar and click enter or 'search'.
+
+# Further information
+
+Interested in learning [Redux](https://www.udemy.com/react-redux/)?
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: "" };
+  }
+
+  _onInputChange(e) {
+    this.setState({ term: e.target.value })
+  }
+
+  _onFormSubmit(e) {
+    e.preventDefault();
+    console.log(e.target);
+  }
+
   render() {
     return (
-      <form className="input-group">
-        <input />
+      <form onSubmit={this._onFormSubmit.bind(this)} className="input-group" >
+        <input
+          placeholder="Get a five-day forcast in your favorite cities"
+          className="form-control"
+          value={this.state.term}
+          onChange={this._onInputChange.bind(this)}
+           />
         <span className="input-group-btn">
           <button type="submit" className="btn btn-secondary">Submit</button>
         </span>
